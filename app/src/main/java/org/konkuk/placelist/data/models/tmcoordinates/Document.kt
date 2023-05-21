f1e159552b184b18c25ab8be7ee10a3709197abf
jaeuk520 <jaeuk520@naver.com>
2023-05-21 20:39:51 +0900

    카카오 로컬 api 관련 클래스 및 인터페이스 생성

---
+++ app/src/main/java/org/konkuk/placelist/data/models/tmcoordinates/Document.kt
@@ -0,0 +1,10 @@
+package org.konkuk.placelist.data.models.tmcoordinates
+
+import com.google.gson.annotations.SerializedName
+
+data class Document(
+    @SerializedName("x")
+    val x: Double?,
+    @SerializedName("y")
+    val y: Double?
+)
---
+++ app/src/main/java/org/konkuk/placelist/data/services/KakaoLocalApiService.kt
@@ -0,0 +1,18 @@
+package org.konkuk.placelist.data.services
+
+import org.konkuk.placelist.data.api.ApiKey.Companion.KAKAO_API_KEY
+import org.konkuk.placelist.data.models.tmcoordinates.TmCoordinatesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface KakaoLocalApiService {
+
+    @Headers("Authorization: KakaoAK $KAKAO_API_KEY")
+    @GET("v2/local/geo/transcoord.json?output_coord=TM")
+    suspend fun getTmCoordinates(
+        @Query("x") longitude: Double,
+        @Query("y") latitude: Double
+    ): Response<TmCoordinatesResponse>
+}

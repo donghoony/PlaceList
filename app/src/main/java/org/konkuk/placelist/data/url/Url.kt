fb531345a147c70492c73a87f4857884210a6530
jaeuk520 <jaeuk520@naver.com>
2023-05-21 20:36:31 +0900

    Url 클래스 생성

---
+++ app/src/main/java/org/konkuk/placelist/data/url/Url.kt
@@ -0,0 +1,8 @@
+package org.konkuk.placelist.data.url
+
+class Url {
+    companion object {
+        const val KAKAO_API_BASE_URL = "https://dapi.kakao.com/"
+        const val PUBLIC_DATA_BASE_URL = "http://apis.data.go.kr/"
+    }
+}

5aad85a1b415b21c0c9ca9df6bc9aa4476a403c0
freelee310 <freelee310@konkuk.ac.kr>
2023-05-26 20:05:41 +0900

    UI 첫번째 commit

--- app/src/main/java/org/konkuk/placelist/IntroActivity.kt
+++ app/src/main/java/org/konkuk/placelist/IntroActivity.kt
@@ -89,7 +89,7 @@ class IntroActivity : AppCompatActivity() {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        binding.dummyButton.setOnTouchListener(delayHideTouchListener)
+        //binding.dummyButton.setOnTouchListener(delayHideTouchListener)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {

7c00e7cbd8c8d0dac0a131aa75ec59d8e1570c78
승재 <jeong77626@gmail.com>
2023-05-28 15:45:24 +0900

    장소구현

--- app/src/main/java/org/konkuk/placelist/AddPlaceDialogFragment.kt
+++ app/src/main/java/org/konkuk/placelist/AddPlaceDialogFragment.kt
@@ -1,16 +1,31 @@
 package org.konkuk.placelist
 
+import android.location.Geocoder
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.activity.viewModels
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import org.konkuk.placelist.databinding.FragmentAddPlaceBinding
+import org.konkuk.placelist.domain.enums.Coordinate
+import java.util.*
 
 class AddPlaceDialogFragment : DialogFragment() {
     lateinit var binding: FragmentAddPlaceBinding
+    //val geocoder= Geocoder(requireActivity())
+    //val myviewModel:MyViewModel by viewModels()
+    val model:MyViewModel by activityViewModels()
+    var text=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         isCancelable = false
@@ -34,6 +49,7 @@ class AddPlaceDialogFragment : DialogFragment() {
     }
 
     private fun initButtons() {
+        val geocoder= Geocoder(requireActivity(), Locale.KOREA)
         with(binding){
             this.closeBtn.setOnClickListener {
                 dismiss()
@@ -42,5 +58,20 @@ class AddPlaceDialogFragment : DialogFragment() {
                 // TODO: Add Place in adapter
             }
         }
+        model.location.observe(viewLifecycleOwner, Observer {
+            binding.location.setText((geocoder.getFromLocation(it.latitude,it.longitude,1))!!.get(0).getAddressLine(0).toString())
+        })
+        binding.location.setOnKeyListener { v, keyCode, event ->
+            if(event.action==KeyEvent.ACTION_DOWN&&keyCode==KEYCODE_ENTER){
+                    val geoaddress =
+                        geocoder.getFromLocationName(binding.location.text.toString(), 1)
+                    val cool: Coordinate =
+                        Coordinate(geoaddress!!.get(0).longitude, geoaddress!!.get(0).latitude)
+                    model.setLiveData(cool)
+
+            }
+            true
+        }
     }
+
 }

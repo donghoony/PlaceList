e0d3d44fca5918eeb8fae4045cb0d6ab422dd5fb
donghoony <aru0504@naver.com>
2023-05-31 16:29:19 +0900

    refactor: Tiramisu 어노테이션 추가

--- app/src/main/java/org/konkuk/placelist/IntroActivity.kt
+++ app/src/main/java/org/konkuk/placelist/IntroActivity.kt
@@ -1,12 +1,15 @@
 package org.konkuk.placelist
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import org.konkuk.placelist.databinding.ActivityIntroBinding
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroBinding
@@ -16,7 +19,7 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        handler.postDelayed(Runnable {
+        handler.postDelayed({
             run{
                 val intent = Intent(this@IntroActivity, MainActivity::class.java)
                 startActivity(intent)

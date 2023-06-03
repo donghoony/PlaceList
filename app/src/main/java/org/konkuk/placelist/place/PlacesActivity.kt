2c678c888f2c384df0964ab997e787ceb7d979f7
jaeuk520 <jaeuk520@naver.com>
2023-06-03 18:00:03 +0900

    feat: recyclerView Swipe delete 구현

--- app/src/main/java/org/konkuk/placelist/place/PlacesActivity.kt
+++ app/src/main/java/org/konkuk/placelist/place/PlacesActivity.kt
@@ -3,10 +3,13 @@ package org.konkuk.placelist.place
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.konkuk.placelist.PlacesListDatabase
 import org.konkuk.placelist.databinding.ActivityPlacesBinding
 import org.konkuk.placelist.domain.Todo
@@ -42,7 +45,28 @@ class PlacesActivity : AppCompatActivity(), AddTodoListener {
             }
             binding.todolist.adapter = todoAdapter
         }
+        val simpleCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
 
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                CoroutineScope(Dispatchers.IO).launch{
+                    db.TodoDao().delete(todoAdapter.items[viewHolder.adapterPosition])
+                    withContext(Dispatchers.Main) {
+                        todoAdapter.removeItem(viewHolder.adapterPosition)
+                    }
+                }
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(simpleCallback)
+        itemTouchHelper.attachToRecyclerView(binding.todolist)
     }
     private fun init(){
         binding.backBtn.setOnClickListener { finish() }

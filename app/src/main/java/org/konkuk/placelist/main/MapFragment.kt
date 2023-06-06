82813a81e4a796a47bc06441f1d3bdcafee0a128
donghoony <aru0504@naver.com>
2023-06-06 13:32:35 +0900

    feat: 마커 클릭 시 범위 설정 기능 추가

--- app/src/main/java/org/konkuk/placelist/main/MapFragment.kt
+++ app/src/main/java/org/konkuk/placelist/main/MapFragment.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.content.ContentValues
 import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
@@ -18,6 +20,9 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
@@ -29,17 +34,17 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     lateinit var mMap: GoogleMap
     private val model : MyViewModel by activityViewModels()
     var binding : FragmentMapBinding? = null
-    lateinit var mLayout : View
     var mLocationManager : LocationManager? = null
     var mLocationListener : LocationListener? = null
     var marker : Marker? = null
+    var radius = 100.0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentMapBinding.inflate(layoutInflater,container,false)
+        binding = FragmentMapBinding.inflate(layoutInflater,container,false)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         return binding!!.root
@@ -53,32 +58,56 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         mMap = p0
+        val iconBitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_pin_red)
+        val resizedIcon = Bitmap.createScaledBitmap(iconBitmap, 60, 72, false)
+        val pinIcon: BitmapDescriptor = BitmapDescriptorFactory.fromBitmap(resizedIcon)
+
+        val circleOptions = CircleOptions()
+            .radius(radius)
+            .fillColor(0x22ff5959)
+            .strokeColor(0xffff5959.toInt())
+            .strokeWidth(5f)
+        val markerOptions = MarkerOptions().icon(pinIcon)
+
         mLocationManager = this.requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager
+        mMap.setOnMarkerClickListener {
+            when(radius){
+                100.0 -> radius = 200.0
+                200.0 -> radius = 500.0
+                500.0 -> radius = 100.0
+            }
+            mMap.clear()
+            val pos = LatLng(it.position.latitude, it.position.longitude)
+            mMap.addCircle(circleOptions.center(pos).radius(radius))
+            mMap.addMarker(markerOptions.position(pos))
+            true
+        }
         mLocationListener = LocationListener { location ->
             val lat = location.latitude
             val lng = location.longitude
             Log.d("GmapViewFragment", "Lat: $lat, lon: $lng")
             val currentLocation = LatLng(lat, lng)
-            marker = mMap.addMarker(MarkerOptions().position(currentLocation))
+            marker = mMap.addMarker(markerOptions.position(currentLocation))
+            mMap.addCircle(circleOptions.center(currentLocation))
+
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
             model.setLiveData(currentLocation)
         }
 
         model.location.observe(viewLifecycleOwner) {
             mMap.clear()
-            marker = mMap.addMarker(MarkerOptions().position(LatLng(it.latitude, it.longitude)))!!
+            marker = mMap.addMarker(markerOptions.position(it))
+            mMap.addCircle(circleOptions.center(it))
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker!!.position, 15.0f))
         }
 
-        var mark : LatLng
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mark, 15.0f))
 
         mMap.setOnMapClickListener {
             Log.d(ContentValues.TAG, "onMapClick :" + it.latitude + it.longitude)
-            mark = LatLng(it.latitude, it.longitude)
             mMap.clear()
-            marker = mMap.addMarker(MarkerOptions().position(mark).title("이름"))!!
-            model.setLiveData(mark)
+            marker = mMap.addMarker(markerOptions.position(it))
+            mMap.addCircle(circleOptions.center(it))
+            model.setLiveData(it)
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker!!.position, 15.0f))
         }
 

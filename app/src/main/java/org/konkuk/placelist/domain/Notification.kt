9e0c34452c984e26ecabe3947aa9a96e431563ad
donghoony <aru0504@naver.com>
2023-06-20 14:28:45 +0900

    refactor: 불필요 파일 제거

--- app/src/main/java/org/konkuk/placelist/domain/Notification.kt
+++
@@ -1,9 +0,0 @@
-package org.konkuk.placelist.domain
-
-import org.konkuk.placelist.domain.enums.NotificationType
-
-class Notification {
-    lateinit var type: NotificationType
-    lateinit var name: String
-    lateinit var detail: String
-}
